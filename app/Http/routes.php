c8f9ad1d9792b118719d3627b2f104e8551ca39c
keisuke.ookuma <kb_kumagki@yahoo.co.jp>
2017-08-23 15:44:50 +0000

    message-board

---
+++ app/Http/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/','MessagesController@index');
+Route::resource('messages','MessagesController');
+
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+//CRUD
+Route::get('messages/{id}','MessagesController@show');
+Route::post('messages','MessagesController@store');
+Route::put('messages/{id}','MessagesController@update');
+Route::delete('messages/{id}','MessagesController@destroy');
+//index:showの補助
+Route::get('messages','MessagesController@index');
+//create:新規作成用のフォームページ
+Route::get('messages/create','MessagesController@create');
+//edit:更新用のフォームページ
+Route::get('messages/{id}/edit','MessagesController@edit');*/
---
+++ resources/views/messages/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+    <h1>id = {{ $message->id }} のメッセージ詳細ページ</h1>
+    
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $message->id }}</td>
+        </tr>
+        <tr>
+            <th>タイトル</th>
+            <td>{{ $message->title }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $message->content }}</td>
+        </tr>
+    </table>
+    <!--<p>タイトル: {{ $message->title }}</p>
+    <p>メッセージ:{{ $message->content }}</p> -->
+    
+    {!! link_to_route('messages.edit', 'このメッセージ編集',['id' => $message->id],['class'=>'btn btn-default']) !!}
+
+    {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除',['class'=>'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
+@endsection
